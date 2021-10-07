98dec4668a40b91239ef8642656b5dce721d6176
bombeo659 <nqt02112001@gmail.com>
2021-10-07 12:33:54 +0700

    update gateway

--- Gateway/aio_config.py
+++ Gateway/aio_config.py
@@ -2,12 +2,12 @@ from Adafruit_IO import MQTTClient
 AIO_USERNAME = "iotg06"
 AIO_KEY = "aio_pZLL43LGAxfNGAXoiLtYy6LCOd0X"
 
-FEED_DOOR = "DOOR"
-FEED_FAN = "FAN"
-FEED_HUMIDITY = "HUMIDITY"
-FEED_LIGHT = "LIGHT"
-FEED_REFRESHER = "REFRESHER"
-FEED_TEMP = "TEMP"
-FEED_WARNING = "WARING"
+FEED_DOOR = "iot.door"
+FEED_FAN = "iot.fan"
+FEED_HUMIDITY = "iot.humidity"
+FEED_LIGHT = "iot.light"
+FEED_REFRESHER = "iot.refresher"
+FEED_TEMP = "iot.temp"
+FEED_WARNING = "iot.warning"
 
 client = MQTTClient(AIO_USERNAME, AIO_KEY)
